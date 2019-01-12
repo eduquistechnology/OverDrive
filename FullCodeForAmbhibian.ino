67bada0f193eb546eb9b71b1d6a0f13eeb217f72
eduquistechnology <46614308+eduquistechnology@users.noreply.github.com>
2019-01-12 17:20:37 +0530

    Create FullCodeForAmbhibian.ino

---
+++ FullCodeForAmbhibian.ino
@@ -0,0 +1,114 @@
+/* include library */
+#include <ESP8266WiFi.h>
+
+/* define port */
+WiFiClient client;
+WiFiServer server(80);
+
+/* WIFI settings */
+const char* ssid = "sagar";
+const char* password = "sagar1234";
+
+/* data received from application */
+String  data =""; 
+
+/* define L298N or L293D motor control pins */
+int leftMotorForward = 2;     /* GPIO2(D4) -> IN3   d4*/
+int rightMotorForward = 15;   /* GPIO15(D8) -> IN1 d8  */
+int leftMotorBackward = 0;    /* GPIO0(D3) -> IN4   d3*/
+int rightMotorBackward = 13;  /* GPIO13(D7) -> IN2  d7*/
+
+
+/* define L298N or L293D enable pins */
+int rightMotorENB = 14; /* GPIO14(D5) -> Motor-A Enable d5 */
+int leftMotorENB = 12;  /* GPIO12(D6) -> Motor-B Enable d6*/
+
+void setup()
+{
+  /* initialize motor control pins as output */
+  pinMode(leftMotorForward, OUTPUT);
+  pinMode(rightMotorForward, OUTPUT); 
+  pinMode(leftMotorBackward, OUTPUT);  
+  pinMode(rightMotorBackward, OUTPUT);
+
+/* start server communication IP*/
+  server.begin();
+  Serial.begin(115200);
+}
+
+void loop()
+{
+    /* If the server available, run the "checkClient" function */  
+    client = server.available();
+    if (!client) return; 
+    data = checkClient ();
+Serial.println(data);
+/************************ Run function according to incoming data from application *************************/
+
+    /* If the incoming data is "forward", run the "MotorForward" function */
+    if (data == "forward") MotorForward();
+    /* If the incoming data is "backward", run the "MotorBackward" function */
+    else if (data == "backward") MotorBackward();
+    /* If the incoming data is "left", run the "TurnLeft" function */
+    else if (data == "left") TurnLeft();
+    /* If the incoming data is "right", run the "TurnRight" function */
+    else if (data == "right") TurnRight();
+    /* If the incoming data is "stop", run the "MotorStop" function */
+    else if (data == "stop") MotorStop();
+} 
+
+/********************************************* FORWARD *****************************************************/
+void MotorForward(void)   
+{
+ 
+  digitalWrite(leftMotorForward,HIGH);
+  digitalWrite(rightMotorForward,HIGH);
+  digitalWrite(leftMotorBackward,LOW);
+  digitalWrite(rightMotorBackward,LOW);
+}
+
+/********************************************* BACKWARD *****************************************************/
+void MotorBackward(void)   
+{
+  
+  digitalWrite(leftMotorBackward,HIGH);
+  digitalWrite(rightMotorBackward,HIGH);
+  digitalWrite(leftMotorForward,LOW);
+  digitalWrite(rightMotorForward,LOW);
+}
+
+/********************************************* TURN LEFT *****************************************************/
+void TurnLeft(void)   
+{
+  digitalWrite(leftMotorForward,LOW);
+  digitalWrite(rightMotorForward,HIGH);
+  digitalWrite(rightMotorBackward,LOW);
+  digitalWrite(leftMotorBackward,HIGH);  
+}
+
+/********************************************* TURN RIGHT *****************************************************/
+void TurnRight(void)   
+{
+  digitalWrite(leftMotorForward,HIGH);
+  digitalWrite(rightMotorForward,LOW);
+  digitalWrite(rightMotorBackward,HIGH);
+  digitalWrite(leftMotorBackward,LOW);
+}
+
+/********************************************* STOP *****************************************************/
+void MotorStop(void)   
+{
+  digitalWrite(leftMotorForward,LOW);
+  digitalWrite(leftMotorBackward,LOW);
+  digitalWrite(rightMotorForward,LOW);
+  digitalWrite(rightMotorBackward,LOW);
+}
+
+String checkClient (void)
+{
+  while(!client.available()) delay(1); 
+  String request = client.readStringUntil('\r');
+  request.remove(0, 5);
+  request.remove(request.length()-9,9);
+  return request;
+}
